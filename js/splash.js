d960fac4474fcc6b5b369d428d195e2620b0d4f9
Ranjith Pachadan <ranjith.pachadan@qburst.com>
2025-03-22 19:08:33 +0530

    initial commits

--- js/splash.js
+++ js/splash.js
@@ -1,7 +1,12 @@
 function initSplashAnimation() {
   const container = document.querySelector(".splash-screen");
+  document.body.classList.add("no-scroll");
+  document.body.setAttribute("data-lenis-prevent", "true"); // Disable lenis scrolling
+
   setTimeout(() => {
     container.style.display = "none";
+    document.body.classList.remove("no-scroll");
+    document.body.removeAttribute("data-lenis-prevent"); // Enable Lenis scrolling
   }, 4000)
 }
 
